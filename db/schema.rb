2e68c028368306ed0b3973a677ee0376f56deae3
marcorambaldi <marco.rambaldi93@gmail.com>
2020-09-11 10:21:14 +0200

    creato contenuti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_152629) do
+ActiveRecord::Schema.define(version: 2020_09_11_080132) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "titolo"
+    t.text "descrizione"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
