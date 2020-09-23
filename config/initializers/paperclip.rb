4063fa1e615e24bcece9b448f472000247773280
marcorambaldi <marco.rambaldi93@gmail.com>
2020-09-23 15:40:47 +0200

    fix1

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 #Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 #Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 'lacermarketmarco.s3.us-east-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'https://lacermarketmarco.s3.us-east-2.amazonaws.com'
